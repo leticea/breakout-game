032d71a3633803689ea2cf6bfd2ea55d24fd0036
leticea <nascimento.leticeah@gmail.com>
2023-05-05 16:28:18 -0300

    feat(script.js): draw ball function

--- script.js
+++ script.js
@@ -57,3 +57,12 @@ function drawPaddle() {
   ctx.fill();
   ctx.closePath();
 }
+
+// Draw ball
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#333";
+  ctx.fill();
+  ctx.closePath();
+}
